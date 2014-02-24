c49d95f882f14eec2555e4922ef67619c484fc88
xupengqi <xupengqi@gmail.com>
2014-02-23 22:54:59 -0800

    2 pages

--- repo.php
+++ repo.php
@@ -33,45 +33,42 @@
         <!-- Collect the nav links, forms, and other content for toggling -->
         <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
         <ul class="nav navbar-nav">
-        <li class="active"><a href="/">Resume</a></li>
-        <li><a href="repo.php">GitHub</a></li>
-        <li class="dropdown">
-            <a href="#" class="dropdown-toggle" data-toggle="dropdown">Dropdown <b class="caret"></b></a>
-            <ul class="dropdown-menu">
-                <li>
-                    <a href="#">Action</a>
-                </li>
-                <li>
-                    <a href="#">Another action</a>
-                </li>
-                <li>
-                    <a href="#">Something else here</a>
-                </li>
-                <li class="divider"></li>
-                <li>
-                    <a href="#">Separated link</a>
-                </li>
-                <li class="divider"></li>
-                <li>
-                    <a href="#">One more separated link</a>
-                </li>
-            </ul>
+        <li>
+        <a href="/">Resume</a>
         </li>
+        <li class="active">
+        <a href="repo.php">GitHub</a>
+        </li>
+        </ul>
         </ul>
         </div><!-- /.navbar-collapse -->
         </div><!-- /.container-fluid -->
         </nav>
 
         <div class="container-fluid">
-            <div class="row">
-                <div class="col-xs-12 text-center">
-                    <iframe style="min-width: 860px; min-height: 1300px; border: none;" src="https://docs.google.com/document/d/1g5qcuRvHUylF-87-TtzEKurTwguRtzdcK49tZ6UlAKY/pub?embedded=true"></iframe>
-                </div>
-
-            </div>
+        <div class="row">
+        <div class="col-xs-12 text-center">
+        <div id="repos" class="list-group"></div>
+        </div>
+        </div>
         </div>
 
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
         <script src="//netdna.bootstrapcdn.com/bootstrap/3.1.1/js/bootstrap.min.js"></script>
+        <script>
+            $(function() {
+                $.getJSON("https://api.github.com/users/xupengqi/repos", function(data) {
+                    var items = [];
+                    $.each(data, function(key, val) {
+                        items.push("<a class='list-group-item' id='" + key + "'>" + val.name + "</a>");
+                    });
+
+                    $("<ul/>", {
+                        "class" : "my-new-list",
+                        html : items.join("")
+                    }).appendTo("#repos");
+                });
+            });
+        </script>
         </body>
 </html>

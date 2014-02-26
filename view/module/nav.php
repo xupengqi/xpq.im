f88a852ab9f43e82f69541c084688eeb431fed23
xupengqi <xupengqi@gmail.com>
2014-02-25 21:35:19 -0800

    link fix

--- view/module/nav.php
+++ view/module/nav.php
@@ -8,7 +8,7 @@
 					class="icon-bar"></span> <span class="icon-bar"></span> <span
 					class="icon-bar"></span>
 			</button>
-			<a class="navbar-brand" href="#">Aaron | Pengqi Xu</a>
+			<a class="navbar-brand" href="/">Aaron | Pengqi Xu</a>
 		</div>
 
 		<!-- Collect the nav links, forms, and other content for toggling -->

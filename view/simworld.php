83db1bc8277cc0134a64fe3ab5827845835cb010
xupengqi <xupengqi@gmail.com>
2014-02-28 08:40:42 -0800

    add blocks

--- view/simworld.php
+++ view/simworld.php
@@ -4,6 +4,11 @@
         <?php echo $this -> renderModule('head'); ?>
         <link rel="stylesheet" href="/css/bootstrap-slider.css">
         <style>
+            #game {
+                position: absolute;
+                left: 0;
+                top: 0;
+            }
             #camerax-wrapper {
                 top: 220px;
                 left: 30px;
@@ -19,32 +24,28 @@
             }
         </style>
     </head>
-    <body>
+    <body style="overflow: hidden;">
         <?php echo $this -> renderModule('nav'); ?>
-
-        <div class="container-fluid">
-            <div class="row" style="position: relative;">
-                <div class="col-xs-12 text-center" id="game"></div>
-                <div id="camerax-wrapper">
-                    <div id="camerax" data-slider-min="0"
-                    data-slider-max="200" data-slider-step="1"
-                    data-slider-value="50"
-                    data-slider-orientation="horizontal"></div>
-                </div>
-                <div id="cameraz-wrapper">
-                    <div id="cameraz" data-slider-min="0"
-                    data-slider-max="200" data-slider-step="1"
-                    data-slider-value="50"
-                    data-slider-orientation="vertical"></div>
-                </div>
-            </div>
+        <div id="game"></div>
+        <div style="position: absolute; top: 0; left: 200px;"><a href="#" id="lock">lock</a></div>
+        <div id="camerax-wrapper">
+            <div id="camerax" data-slider-min="0"
+            data-slider-max="200" data-slider-step="1"
+            data-slider-value="50"
+            data-slider-orientation="horizontal"></div>
+        </div>
+        <div id="cameraz-wrapper">
+            <div id="cameraz" data-slider-min="0"
+            data-slider-max="200" data-slider-step="1"
+            data-slider-value="50"
+            data-slider-orientation="vertical"></div>
         </div>
 
         <?php echo $this -> renderModule('js'); ?>
         <?php echo $this -> renderModule('debug'); ?>
         <script src="/js/three.min.js"></script>
-
         <script src="/js/stats.min.js"></script>
+        <script src="/js/PointerLockControls.js"></script>
         <script src="/js/bootstrap-slider.js"></script>
 
         <script>
@@ -56,12 +57,22 @@
             });
 
             var stats;
-            var camera, scene, renderer;
-            var camerax = 50, cameraz = 50;
+            var camera, scene, renderer, controls,time = Date.now();
+
+            var plane, rollOverMesh;
+
+            var mouse2D, projector, raycaster, isMouseDown = false, isShiftDown = false, isDragging = false;
+
+            var voxelPosition = new THREE.Vector3(), tmpVec = new THREE.Vector3(), normalMatrix = new THREE.Matrix3();
+            var cubeGeo, cubeMaterial;
+
+            var camerax = 50, cameraz = 50, cameray = 100;
             var gridSize = 500, step = 20;
             var windowHalfX = window.innerWidth / 2;
             var windowHalfY = window.innerHeight / 2;
             var lookAt = new THREE.Vector3(0, 0, 0);
+
+            var objects = [];
 
             init();
             animate();
@@ -69,6 +80,10 @@
             function init() {
                 // Camera & Scene
                 camera = new THREE.OrthographicCamera(window.innerWidth / -2, window.innerWidth / 2, window.innerHeight / 2, window.innerHeight / -2, -500, 1000);
+
+                //camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 1, 10000 );
+                //camera.position.y = 800;
+
                 scene = new THREE.Scene();
 
                 // Grid
@@ -90,16 +105,23 @@
                 scene.add(line);
 
                 // Cubes
-                var geometry = new THREE.BoxGeometry(step, step, step);
-                var material = new THREE.MeshLambertMaterial({
-                    color : 0xffffff,
+                cubeGeo = new THREE.BoxGeometry(step, step, step);
+                /*var material = new THREE.MeshLambertMaterial({
+                 color : 0xffffff,
+                 shading : THREE.FlatShading,
+                 overdraw : 0.5
+                 });*/
+                cubeMaterial = new THREE.MeshLambertMaterial({
+                    color : 0xfeb74c,
+                    ambient : 0x00ff80,
                     shading : THREE.FlatShading,
-                    overdraw : 0.5
+                    map : THREE.ImageUtils.loadTexture("/img/square-outline-textured.png")
                 });
+                cubeMaterial.ambient = cubeMaterial.color;
 
                 var cubes = {};
-                for (var i = 0; i < 200; i++) {
-                    var cube = new THREE.Mesh(geometry, material);
+                for (var i = 0; i < 20; i++) {
+                    var cube = new THREE.Mesh(cubeGeo, cubeMaterial);
                     cube.scale.y = Math.floor(Math.random() * 2 + 1);
                     cube.position.x = Math.floor((Math.random() * gridSize - gridSize / 2 ) / step) * step + step / 2;
                     cube.position.y = (cube.scale.y * step ) / 2;
@@ -111,33 +133,77 @@
                         }
                         cubes[cube.position.x][cube.position.z] = true;
                         scene.add(cube);
-                    } else {
-                        console.log("cube exists");
+                        objects.push(cube);
                     }
                 }
-                console.log(cubes);
+
+                // ground
+                var initColor = new THREE.Color(0x497f13);
+                var initTexture = THREE.ImageUtils.generateDataTexture(1, 1, initColor);
+                var groundMaterial = new THREE.MeshPhongMaterial({
+                    color : 0xffffff,
+                    specular : 0x111111,
+                    map : initTexture
+                });
+                var groundTexture = THREE.ImageUtils.loadTexture("/img/grasslight-big.jpg", undefined, function() {
+                    groundMaterial.map = groundTexture
+                });
+                groundTexture.wrapS = groundTexture.wrapT = THREE.RepeatWrapping;
+                groundTexture.repeat.set(25, 25);
+                groundTexture.anisotropy = 16;
+
+                plane = new THREE.Mesh(new THREE.PlaneGeometry(1000, 1000), groundMaterial);
+                plane.rotation.x = -Math.PI / 2;
+                plane.visible = true;
+                scene.add(plane);
+                objects.push(plane);
+
+                // roll-over helpers
+                var rollOverGeo = new THREE.BoxGeometry(step, step, step);
+                var rollOverMaterial = new THREE.MeshBasicMaterial({
+                    color : 0xff0000,
+                    opacity : 0.5,
+                    transparent : true
+                });
+                rollOverMesh = new THREE.Mesh(rollOverGeo, rollOverMaterial);
+                scene.add(rollOverMesh);
 
                 // Lights
-                var ambientLight = new THREE.AmbientLight(Math.random() * 0x10);
-                scene.add(ambientLight);
+                var light, materials;
+                scene.add(new THREE.AmbientLight(0x666666));
+                light = new THREE.DirectionalLight(0xdfebff, 1.75);
+                light.position.set(50, 200, 100);
+                light.position.multiplyScalar(1.3);
+                light.castShadow = true;
+                //light.shadowCameraVisible = true;
+                light.shadowMapWidth = 2048;
+                light.shadowMapHeight = 2048;
+                var d = 300;
+                light.shadowCameraLeft = -d;
+                light.shadowCameraRight = d;
+                light.shadowCameraTop = d;
+                light.shadowCameraBottom = -d;
+                light.shadowCameraFar = 1000;
+                light.shadowDarkness = 0.5;
+                scene.add(light);
 
-                var directionalLight = new THREE.DirectionalLight(Math.random() * 0xffffff);
-                directionalLight.position.x = Math.random() - 0.5;
-                directionalLight.position.y = Math.random() - 0.5;
-                directionalLight.position.z = Math.random() - 0.5;
-                directionalLight.position.normalize();
-                scene.add(directionalLight);
+                light = new THREE.DirectionalLight(0x3dff0c, 0.35);
+                light.position.set(0, -1, 0);
+                scene.add(light);
+                
+                // PointerLockControl
+                controls = new THREE.PointerLockControls( camera );
+                scene.add( controls.getObject() );
 
-                var directionalLight = new THREE.DirectionalLight(Math.random() * 0xffffff);
-                directionalLight.position.x = Math.random() - 0.5;
-                directionalLight.position.y = Math.random() - 0.5;
-                directionalLight.position.z = Math.random() - 0.5;
-                directionalLight.position.normalize();
-                scene.add(directionalLight);
+                /*renderer = new THREE.CanvasRenderer();
+                 renderer.setClearColor(0xf0f0f0);
+                 renderer.setSize(window.innerWidth - 100, window.innerHeight - 100);*/
 
-                renderer = new THREE.CanvasRenderer();
+                renderer = new THREE.WebGLRenderer({
+                    antialias : true
+                });
                 renderer.setClearColor(0xf0f0f0);
-                renderer.setSize(window.innerWidth - 100, window.innerHeight - 100);
+                renderer.setSize(window.innerWidth, window.innerHeight);
 
                 $("#game").append(renderer.domElement);
 
@@ -146,7 +212,16 @@
                 stats.domElement.style.top = '0px';
                 $("#game").append(stats.domElement);
 
+                mouse2D = new THREE.Vector3(0, 10000, 0.5);
+                projector = new THREE.Projector();
+
                 window.addEventListener('resize', onWindowResize, false);
+                
+                $("#lock").click(function() {
+                    // Ask the browser to lock the pointer
+                    document.body.requestPointerLock = document.body.requestPointerLock || document.body.mozRequestPointerLock || document.body.webkitRequestPointerLock;
+                    document.body.requestPointerLock();
+                });
             }
 
             function onWindowResize() {
@@ -156,58 +231,175 @@
                 camera.bottom = window.innerHeight / -2;
                 camera.updateProjectionMatrix();
                 //TODO: WHAT IS THIS
-                renderer.setSize(window.innerWidth - 100, window.innerHeight - 100);
+                renderer.setSize(window.innerWidth, window.innerHeight);
             }
 
             function animate() {
                 requestAnimationFrame(animate);
-                //TODO: WHAT IS THIS
+
+                raycaster = projector.pickingRay(mouse2D.clone(), camera);
+                var intersects = raycaster.intersectObjects(objects);
+                if (intersects.length > 0) {
+                    //console.log(intersects.length);
+                    setVoxelPosition(intersects[0]);
+                    rollOverMesh.position = voxelPosition;
+                }
+
                 camera.position.x = camerax;
                 camera.position.z = cameraz;
-                camera.position.y = 100;
+                camera.position.y = cameray;
                 camera.lookAt(lookAt);
-                //TODO: on mouse drag move this
+
+                controls.update( Date.now() - time );
                 renderer.render(scene, camera);
                 stats.update();
+                time = Date.now();
             }
 
+                var pointerLockError = function ( event ) {
+                    console.log(event);
+
+                };
 
             document.addEventListener('mousedown', onDocumentMouseDown, false);
+            document.addEventListener('mousemove', onDocumentMouseMove, false);
+            document.addEventListener('keydown', onDocumentKeyDown, false);
+            document.addEventListener('keyup', onDocumentKeyUp, false);
+                document.addEventListener( 'pointerlockchange', pointerLockChange, false );
+                document.addEventListener( 'mozpointerlockchange', pointerLockChange, false );
+                document.addEventListener( 'webkitpointerlockchange', pointerLockChange, false );
+                document.addEventListener( 'pointerlockerror', pointerLockError, false );
+                document.addEventListener( 'mozpointerlockerror', pointerLockError, false );
+                document.addEventListener( 'webkitpointerlockerror', pointerLockError, false );
+
 
             function onDocumentMouseDown(event) {
                 event.preventDefault();
-                document.addEventListener('mousemove', onDocumentMouseMove, false);
                 document.addEventListener('mouseup', onDocumentMouseUp, false);
                 document.addEventListener('mouseout', onDocumentMouseOut, false);
                 mouseXOnMouseDown = event.clientX - windowHalfX;
                 mouseYOnMouseDown = event.clientY - windowHalfY;
-
+                isMouseDown = true;
             }
 
             function onDocumentMouseMove(event) {
-                mouseX = event.clientX - windowHalfX;
-                mouseY = event.clientY - windowHalfY;
-                lookAt.z += (mouseX - mouseXOnMouseDown) * .03;
-                lookAt.x -= (mouseX - mouseXOnMouseDown) * .03;
-                cameraz += (mouseX - mouseXOnMouseDown) * .03;
-                camerax -= (mouseX - mouseXOnMouseDown) * .03;
+                if (isMouseDown) {
+                    /*mouseX = event.clientX - windowHalfX;
+                    mouseY = event.clientY - windowHalfY;
+                    lookAt.z += (mouseX - mouseXOnMouseDown) * .03;
+                    lookAt.x -= (mouseX - mouseXOnMouseDown) * .03;
+                    cameraz += (mouseX - mouseXOnMouseDown) * .03;
+                    camerax -= (mouseX - mouseXOnMouseDown) * .03;
 
-                lookAt.z -= (mouseY - mouseYOnMouseDown) * .03;
-                lookAt.x -= (mouseY - mouseYOnMouseDown) * .03;
-                cameraz -= (mouseY - mouseYOnMouseDown) * .03;
-                camerax -= (mouseY - mouseYOnMouseDown) * .03;
+                    lookAt.z -= (mouseY - mouseYOnMouseDown) * .03;
+                    lookAt.x -= (mouseY - mouseYOnMouseDown) * .03;
+                    cameraz -= (mouseY - mouseYOnMouseDown) * .03;
+                    camerax -= (mouseY - mouseYOnMouseDown) * .03;*/
+                   
+                    isDragging = true;
+                    rollOverMesh.visible = false;
+
+                }
+
+                mouse2D.x = (event.clientX / window.innerWidth ) * 2 - 1;
+                mouse2D.y = -(event.clientY / window.innerHeight ) * 2 + 1;
+
             }
 
             function onDocumentMouseUp(event) {
-                document.removeEventListener('mousemove', onDocumentMouseMove, false);
+                document.removeEventListener('mouseup', onDocumentMouseUp, false);
+                document.removeEventListener('mouseout', onDocumentMouseOut, false);
+
+                if (!isDragging) {
+                    var intersects = raycaster.intersectObjects(objects);
+                    if (intersects.length > 0) {
+                        var intersector = intersects[0];
+                        // delete cube
+                        if (isShiftDown) {
+                            if (intersector.object != plane) {
+                                scene.remove(intersector.object);
+                                objects.splice(objects.indexOf(intersector.object), 1);
+                            }
+                        } else {
+                            setVoxelPosition(intersector);
+                            //var voxel = new THREE.Mesh(cubeGeo, cubeMaterial);
+                            var voxel = getDirt();
+                            console.log(voxelPosition);
+                            voxel.position.copy(voxelPosition);
+                            voxel.position.y = 1;
+                            voxel.matrixAutoUpdate = false;
+                            voxel.updateMatrix();
+                            scene.add(voxel);
+                            objects.push(voxel);
+                        }
+                    }
+                }
+
+                isMouseDown = false;
+                isDragging = false;
+                rollOverMesh.visible = true;
+            }
+            
+            function getDirt() {
+                
+                var initColor = new THREE.Color(0x497f13);
+                var initTexture = THREE.ImageUtils.generateDataTexture(1, 1, initColor);
+                var groundMaterial = new THREE.MeshPhongMaterial({
+                    color : 0xffffff,
+                    specular : 0x111111,
+                    map : initTexture
+                });
+                var groundTexture = THREE.ImageUtils.loadTexture("/img/backgrounddetailed6.jpg", undefined, function() {
+                    groundMaterial.map = groundTexture
+                });
+                groundTexture.wrapS = groundTexture.wrapT = THREE.RepeatWrapping;
+                groundTexture.repeat.set(25, 25);
+                groundTexture.anisotropy = 16;
+
+                var dirt = new THREE.Mesh(new THREE.PlaneGeometry(step, step), groundMaterial);
+                dirt.rotation.x = -Math.PI / 2;
+                return dirt;
+            }
+
+            function onDocumentMouseOut(event) {
                 document.removeEventListener('mouseup', onDocumentMouseUp, false);
                 document.removeEventListener('mouseout', onDocumentMouseOut, false);
             }
 
-            function onDocumentMouseOut(event) {
-                document.removeEventListener('mousemove', onDocumentMouseMove, false);
-                document.removeEventListener('mouseup', onDocumentMouseUp, false);
-                document.removeEventListener('mouseout', onDocumentMouseOut, false);
+            function onDocumentKeyDown(event) {
+                switch( event.keyCode ) {
+                    case 16:
+                        isShiftDown = true;
+                        rollOverMesh.visible = false;
+                        break;
+                }
+            }
+
+            function onDocumentKeyUp(event) {
+                switch ( event.keyCode ) {
+                    case 16:
+                        isShiftDown = false;
+                        rollOverMesh.visible = true;
+                        break;
+                }
+            }
+
+            function setVoxelPosition(intersector) {
+                normalMatrix.getNormalMatrix(intersector.object.matrixWorld);
+                tmpVec.copy(intersector.face.normal);
+                tmpVec.applyMatrix3(normalMatrix).normalize();
+                voxelPosition.addVectors(intersector.point, tmpVec);
+                voxelPosition.divideScalar(step).floor().multiplyScalar(step).addScalar(step / 2);
+                //console.log(voxelPosition.z);
+            }
+
+            function pointerLockChange(event) {
+                console.log("pointerLockChange");
+                if (document.pointerLockElement === document.body || document.mozPointerLockElement === document.body || document.webkitPointerLockElement === document.body) {
+                    controls.enabled = true;
+                } else {
+                    controls.enabled = false;
+                }
             }
 
             var debugaxis = function(axisLength) {
